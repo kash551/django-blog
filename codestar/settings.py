d68bc0f27b7235152c75a79f0bca9406d258d08c
kash hussain <kashifh2005@gmail.com>
2024-06-12 15:08:20 +0000

    stuck on crsf

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kash551-djangoblog-ry58mfhizkf.ws-eu114.gitpod.io',
                  '.herokuapp.com'
@@ -90,8 +90,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("postgres://ulddcpli3y8:jc1lnbCGucuM@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/dig_cold_giver_64829"))
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
